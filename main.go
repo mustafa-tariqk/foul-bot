f4b9fdfc05b9f5ce52ce6fec7a2ef3db3df77a1d
mustafa-tariqk <mustafa.tariqk@icloud.com>
2024-11-23 19:40:31 -0800

    cleanup2

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -12,129 +13,62 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main() {
-	godotenv.Load()
-	guildId := os.Getenv("DISCORD_GUILD_ID")
-	token := os.Getenv("DISCORD_TOKEN")
-	appId := os.Getenv("DISCORD_APPLICATION_ID")
+var (
+	POINTS_JSON            = "points.json"
+	DISCORD_TOKEN          = "DISCORD_TOKEN"
+	DISCORD_GUILD_ID       = "DISCORD_GUILD_ID"
+	DISCORD_APPLICATION_ID = "DISCORD_APPLICATION_ID"
+)
 
+func loadPoints() map[string]int64 {
 	points := make(map[string]int64)
 
-	bot, err := discordgo.New("Bot " + token)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	bot.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		if i.Type == discordgo.InteractionApplicationCommand {
-			switch i.ApplicationCommandData().Name {
-			case "own":
-				// Retrieve the command options
-				options := i.ApplicationCommandData().Options
-				user := options[0].UserValue(s)
-				number := options[1].IntValue()
-				reason := options[2].StringValue()
-
-				// Format the response message
-				response := fmt.Sprintf("Own <@%s> %+d for %s", user.ID, number, reason)
-
-				// Create buttons
-				owningButton := discordgo.Button{
-					Label:    "Owning",
-					Style:    discordgo.PrimaryButton,
-					CustomID: "owning",
-				}
-				notOwningButton := discordgo.Button{
-					Label:    "Not Owning",
-					Style:    discordgo.SecondaryButton,
-					CustomID: "not_owning",
-				}
-
-				// Create action row
-				actionRow := discordgo.ActionsRow{
-					Components: []discordgo.MessageComponent{
-						owningButton,
-						notOwningButton,
-					},
-				}
-
-				// Create interaction response data
-				responseData := discordgo.InteractionResponseData{
-					Content:    response,
-					Components: []discordgo.MessageComponent{actionRow},
-				}
-
-				// Create interaction response
-				interactionResponse := discordgo.InteractionResponse{
-					Type: discordgo.InteractionResponseChannelMessageWithSource,
-					Data: &responseData,
-				}
-
-				// Respond to the interaction
-				s.InteractionRespond(i.Interaction, &interactionResponse)
-
-				// TODO: add to counter ~after~ poll
-				points[user.ID] += number
-			case "leaderboard":
-				type userPoints struct {
-					userID string
-					points int64
-				}
-				pairs := make([]userPoints, 0, len(points))
-				for id, score := range points {
-					pairs = append(pairs, userPoints{id, score})
-				}
-
-				// Sort by points descending
-				sort.Slice(pairs, func(i, j int) bool {
-					return pairs[i].points > pairs[j].points
-				})
-
-				// Get top 10 or all if less
-				if len(pairs) > 10 {
-					pairs = pairs[:10]
-				}
-
-				// Number emojis for ranking
-				numbers := []string{":one:", ":two:", ":three:", ":four:", ":five:", ":six:", ":seven:", ":eight:", ":nine:", ":ten:"}
-
-				description := ""
-				for i, pair := range pairs {
-					user, err := s.User(pair.userID)
-					if err != nil {
-						fmt.Printf("Error fetching user: %v\n", err)
-						continue
-					}
-					description += fmt.Sprintf("%s %s: %d\n", numbers[i], user.Username, pair.points)
-				}
-
-				embed := &discordgo.MessageEmbed{
-					Title:       "Leaderboard",
-					Description: description,
-				}
-
-				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-					Type: discordgo.InteractionResponseChannelMessageWithSource,
-					Data: &discordgo.InteractionResponseData{
-						Embeds: []*discordgo.MessageEmbed{embed},
-					},
-				})
-			}
+	if _, err := os.Stat(POINTS_JSON); os.IsNotExist(err) {
+		data, err := json.Marshal(points)
+		if err != nil {
+			log.Fatalf("could not marshal empty points: %s", err)
 		}
-	})
+		if err := os.WriteFile(POINTS_JSON, data, 0644); err != nil {
+			log.Fatalf("could not create points file: %s", err)
+		}
+	} else {
+		data, err := os.ReadFile(POINTS_JSON)
+		if err != nil {
+			log.Fatalf("could not read points file: %s", err)
+		}
+		if err := json.Unmarshal(data, &points); err != nil {
+			log.Fatalf("could not unmarshal points: %s", err)
+		}
+	}
 
-	bot.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+	return points
+}
 
-	err = bot.Open()
+func loadEnv() (*discordgo.Session, map[string]int64, string, string) {
+	godotenv.Load()
+	bot, err := discordgo.New("Bot " + os.Getenv(DISCORD_TOKEN))
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer bot.Close()
+	points := loadPoints()
 
+	guildId := os.Getenv(DISCORD_GUILD_ID)
+	appId := os.Getenv(DISCORD_APPLICATION_ID)
+
+	return bot, points, guildId, appId
+}
+
+func savePoints(points map[string]int64) {
+	data, err := json.Marshal(points)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("could not marshal points: %s", err)
 	}
+	if err := os.WriteFile(POINTS_JSON, data, 0644); err != nil {
+		log.Fatalf("could not write points file: %s", err)
+	}
+}
 
+func establishCommands(bot *discordgo.Session, guildId string, appId string) {
 	commands := []*discordgo.ApplicationCommand{
 		{
 			Name:        "own",
@@ -167,13 +101,91 @@ func main() {
 		},
 	}
 
-	_, err = bot.ApplicationCommandBulkOverwrite(appId, guildId, commands)
+	_, err := bot.ApplicationCommandBulkOverwrite(appId, guildId, commands)
 	if err != nil {
 		log.Fatalf("could not register commands: %s", err)
 	}
 
-	fmt.Println("Bot is running")
+	bot.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+}
 
+func create_leaderboard(points map[string]int64, s *discordgo.Session) *discordgo.MessageEmbed {
+	type userPoints struct {
+		userID string
+		points int64
+	}
+	pairs := make([]userPoints, 0, len(points))
+	for id, score := range points {
+		pairs = append(pairs, userPoints{id, score})
+	}
+
+	sort.Slice(pairs, func(i, j int) bool {
+		return pairs[i].points > pairs[j].points
+	})
+
+	if len(pairs) > 10 {
+		pairs = pairs[:10]
+	}
+
+	numbers := []string{":one:", ":two:", ":three:", ":four:", ":five:", ":six:", ":seven:", ":eight:", ":nine:", ":ten:"}
+
+	description := ""
+	for i, pair := range pairs {
+		user, err := s.User(pair.userID)
+		if err != nil {
+			fmt.Printf("Error fetching user: %v\n", err)
+			continue
+		}
+		description += fmt.Sprintf("%s %s: %d\n", numbers[i], user.Username, pair.points)
+	}
+
+	embed := &discordgo.MessageEmbed{
+		Title:       "Leaderboard",
+		Description: description,
+	}
+
+	return embed
+}
+
+func main() {
+	bot, points, guildId, appId := loadEnv()
+
+	bot.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if i.Type == discordgo.InteractionApplicationCommand {
+			options := i.ApplicationCommandData().Options
+			switch i.ApplicationCommandData().Name {
+			case "own":
+				// Retrieve the command options
+				user := options[0].UserValue(s)
+				number := options[1].IntValue()
+				reason := options[2].StringValue()
+
+				fmt.Printf("Own <@%s> %+d for %s\n", user.ID, number, reason)
+
+				// TODO: add to counter ~after~ poll
+				points[user.ID] += number
+				savePoints(points)
+			case "leaderboard":
+				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Embeds: []*discordgo.MessageEmbed{create_leaderboard(points, s)},
+					},
+				})
+			}
+		}
+	})
+
+	err := bot.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer bot.Close()
+
+	establishCommands(bot, guildId, appId)
+	fmt.Println("Bot is running...")
+
+	// Runs forever, shut down with ctrl+c
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
